66a9d6c8e06c66d006740cdbbdbdcbc08b02c0a0
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-07-23 13:54:59 -0600

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/:characterName" element={<CharacterPage />} />
+          <Route path="/:CharacterName" element={<CharacterPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/CharacterPage/CharacterPage.tsx
+++ src/pages/CharacterPage/CharacterPage.tsx
@@ -1,4 +1,14 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+
 function CharacterPage() {
+  const { CharacterName } = useParams();
+  const [character, setCharacter] = useState(null);
+
+  if (character === null) {
+    return <p>Loading Character...</p>;
+  }
+
   return (
     <>
       <p>Hello</p>
