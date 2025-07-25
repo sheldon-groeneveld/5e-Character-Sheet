279a1551aa6576716edfe1b85d90d5d06ad31aa0
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-07-25 16:10:37 -0600

    setup defaults and styles

--- src/components/CharacterInfo/CharacterInfo.tsx
+++ src/components/CharacterInfo/CharacterInfo.tsx
@@ -1,14 +1,30 @@
+import "./CharacterInfo.scss";
+
 function CharacterInfo() {
   return (
     <section className="character-info">
-      <div className="character-info__name"></div>
+      <div className="character-info__name">
+        <p>Ipsum 1</p>
+      </div>
       <div className="character-info__wrapper">
-        <div className="character-info__class-and-level"></div>
-        <div className="character-info__race"></div>
-        <div className="character-info__background"></div>
-        <div className="character-info__alignment"></div>
-        <div className="character-info__player"></div>
-        <div className="character-info__experience"></div>
+        <div className="character-info__item">
+          <p>Ipsum 2</p>
+        </div>
+        <div className="character-info__item">
+          <p>Ipsum 3</p>
+        </div>
+        <div className="character-info__item">
+          <p>Ipsum 4</p>
+        </div>
+        <div className="character-info__item">
+          <p>Ipsum 5</p>
+        </div>
+        <div className="character-info__item">
+          <p>Ipsum 6</p>
+        </div>
+        <div className="character-info__item">
+          <p>Ipsum 7</p>
+        </div>
       </div>
     </section>
   );
