2839b76a7d9533284b229924c50ef1f8a10bd4f4
sheldon-groeneveld <sheldongroeneveld@gmail.com>
2025-07-28 15:04:55 -0600

    add inputs, labels, and styling to character info section

--- src/components/CharacterInfo/CharacterInfo.tsx
+++ src/components/CharacterInfo/CharacterInfo.tsx
@@ -4,26 +4,73 @@ function CharacterInfo() {
   return (
     <section className="character-info">
       <div className="character-info__name">
-        <p>Ipsum 1</p>
+        <input
+          type="text"
+          id="character-name"
+          className="character-info__input"
+        />
+        <label htmlFor="character-name" className="character-info__label">
+          Character Name
+        </label>
       </div>
+
       <div className="character-info__wrapper">
         <div className="character-info__item">
-          <p>Ipsum 2</p>
+          <input
+            type="text"
+            id="class-and-level"
+            className="character-info__input"
+          />
+          <label htmlFor="class-and-level" className="character-info__label">
+            Class & Level
+          </label>
         </div>
+
         <div className="character-info__item">
-          <p>Ipsum 3</p>
+          <input
+            type="text"
+            id="background"
+            className="character-info__input"
+          />
+          <label htmlFor="background" className="character-info__label">
+            Background
+          </label>
         </div>
+
         <div className="character-info__item">
-          <p>Ipsum 4</p>
+          <input
+            type="text"
+            id="player-name"
+            className="character-info__input"
+          />
+          <label htmlFor="player-name" className="character-info__label">
+            Player Name
+          </label>
         </div>
+
         <div className="character-info__item">
-          <p>Ipsum 5</p>
+          <input type="text" id="race" className="character-info__input" />
+          <label htmlFor="race" className="character-info__label">
+            Race
+          </label>
         </div>
+
         <div className="character-info__item">
-          <p>Ipsum 6</p>
+          <input type="text" id="alignment" className="character-info__input" />
+          <label htmlFor="alignment" className="character-info__label">
+            Alignment
+          </label>
         </div>
+
         <div className="character-info__item">
-          <p>Ipsum 7</p>
+          <input
+            type="text"
+            id="experience-points"
+            className="character-info__input"
+          />
+          <label htmlFor="experience-points" className="character-info__label">
+            Experience Points
+          </label>
         </div>
       </div>
     </section>
